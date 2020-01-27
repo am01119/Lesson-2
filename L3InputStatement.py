41f1aa798796fb76e0ab6f787344a6d8f01d6454
am01119 <58179951+am01119@users.noreply.github.com>
2020-01-27 18:46:16 +0100

    Add files via upload

---
+++ L3InputStatement.py
@@ -0,0 +1,7 @@
+print("Hello")
+country = input("Please enter the name of your country: ")
+president = input("Please enter the name of your president: ")
+
+print(f'The name of your country is {country} and the name of your president is {president}')
+
+input() 
