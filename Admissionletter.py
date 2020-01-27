c41eb9cfb60fa62979ec8da9441cae9427235e8a
am01119 <58179951+am01119@users.noreply.github.com>
2020-01-27 18:45:54 +0100

    Add files via upload

---
+++ Admissionletter.py
@@ -0,0 +1,8 @@
+print("Hello")
+
+score = int(input("Please enter your jamp score: \n"))
+credits = int(input("Please enter your number of credits: \n"))
+
+print(score > 250 and credits > 5) 
+
+input()
