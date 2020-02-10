a39610fba454a43d326c199df547d51845420ebe
am01119 <58179951+am01119@users.noreply.github.com>
2020-02-10 20:29:31 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,12 @@
+def welcome(name ='John Doe'):
+    """
+    DOCSTRING
+    A function that says welcome
+    Accepts no argument
+    """
+    print(f'{name} ,You are welcome here.')
+
+welcome()
+
+welcome('Alameen')
+        
